a28d1b9543869c5117c3cfeceaff6e9b050b5701
Pedro Lucas Santos Ferreira <pedrolucassantosferreira426@gmail.com>
2023-01-14 19:16:04 -0300

    novo: cors.

--- index.ts
+++ index.ts
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser');
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 app.use(express.static('public'));
