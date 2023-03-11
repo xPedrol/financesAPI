dd9180343c40ef4295e342e43261290632590bcf
Pedro Lucas Santos Ferreira <pedrolucassantosferreira426@gmail.com>
2023-03-11 14:17:43 -0300

    atualização: paginate aceita perPage e pageSize.

--- utils/handlePagination.utils.ts
+++ utils/handlePagination.utils.ts
@@ -1,6 +1,8 @@
 export const getPaginationParams = (
   obj: any
-): { page: number | null; perPage: number | null } => ({
-  page: obj.page || null,
-  perPage: obj.perPage || null,
-});
+): { page: number | null; perPage: number | null } => {
+  const page = obj.page ? parseInt(obj.page as string) : null;
+  const perPage = obj.perPage ? parseInt(obj.perPage as string) : null;
+  const pageSize = obj.pageSize ? parseInt(obj.pageSize as string) : null;
+  return { page, perPage: pageSize || perPage };
+};
